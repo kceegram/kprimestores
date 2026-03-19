3d34955dc35ba487318a4c6f1dc30e1723f50132
kceegram <158181757+kceegram@users.noreply.github.com>
2026-03-20 00:28:41 +0100

    NEW

--- app/ELECTRONICS/page.js
+++ app/ELECTRONICS/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Page = () => {
+const ElectronicsPage = () => {
   return (
     <div>
       
@@ -8,7 +8,4 @@ const Page = () => {
   )
 }
 
-export default Page
-
-
-
+export default ElectronicsPage
